cc302e6166d7529159b297a20a24da9aabe297b8
Stephen <73615623+smcgdub@users.noreply.github.com>
2022-06-16 15:45:24 +0100

    Changed the alaert message if names field left blank

--- assets/JavaScript/app.js
+++ assets/JavaScript/app.js
@@ -5,9 +5,9 @@ function addTo() {
   myarray.push(...document.getElementById("names").value.split(","));
   let number_of_names = myarray.length;
   pick_who_pays = Math.floor(Math.random() * number_of_names);
-  let whoIsPaying = document.getElementById("who_is_paying").innerHTML = "Bad news " + myarray[pick_who_pays] + " looks like you're paying the bill! Better luck next time";
+  let whoIsPaying = document.getElementById("who_is_paying").innerHTML = "Bad news " + myarray[pick_who_pays] + ", looks like you're paying the bill! Better luck next time";
 } else {
-  alert("Please enter some names");
+  alert("You must enter some names");
 }}
 
 // Reset button and function 
@@ -16,4 +16,3 @@ resetButton = document.getElementById("reset_button");
 function reloadPage() {
   window.location.reload();
 }
-
