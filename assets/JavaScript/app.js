5365e8c6e835547bb67effb3d4ee8c18ec7e2673
Stephen <73615623+smcgdub@users.noreply.github.com>
2022-06-16 15:28:50 +0100

    Added if statement to the addTo() function so users must enter a name or function wont excecute

--- assets/JavaScript/app.js
+++ assets/JavaScript/app.js
@@ -1,11 +1,14 @@
-let myarray = []
+let myarray = [];
 
 function addTo() {
+  if(document.getElementById("names").value != "") {
   myarray.push(...document.getElementById("names").value.split(","));
   let number_of_names = myarray.length;
   pick_who_pays = Math.floor(Math.random() * number_of_names);
   let whoIsPaying = document.getElementById("who_is_paying").innerHTML = "Bad news " + myarray[pick_who_pays] + " looks like you're paying the bill! Better luck next time";
-}
+} else {
+  alert("Please enter some names");
+}}
 
 // Reset button and function 
 resetButton = document.getElementById("reset_button");
@@ -13,3 +16,4 @@ resetButton = document.getElementById("reset_button");
 function reloadPage() {
   window.location.reload();
 }
+
